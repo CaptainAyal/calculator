eabea0df1f04509c47280820896176ccc95a2e5f
CaptainAyal <ayalmesher@gmail.com>
2025-02-25 11:21:12 +0200

    Git file got corrupted. back up reloaded

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# calculator
+Final TOP Project - Create a calculator
+
+The purpose of this project is to showcase all topics learned in the Foundations course.
+
+Create a calculator that can complete simple calculations and account for user errors.
+
+Calculator must:
+    Add, subtract, multiply, divide
+    Have an HTML interface
+        Include buttons for all digits and operators
+        Include button for clear
+        include display
+
+Account for
+    Users entering multiple operators
+    Round long decimals
+    When is "=" pressed
+    clear correctly
+    divide by "0"
+    Clear results after displaying them instead of continuing with the same calculation
---
+++ js.js
@@ -0,0 +1,56 @@
+// create function for addition
+const add = (a, b)=>{return a+b};
+
+// create function for subtraction
+const subtract = (a, b)=>{return a-b};
+
+// create function for multiplication
+const multiply = (a, b)=>{return a*b};
+
+// create function for division
+const divide = (a, b)=>{return a/b};
+
+//create variables
+let num1=0;
+let num2=0;
+let operator;
+let display=0;
+
+//create function "operate" that calls the above functions using the variables
+const operate = function(a, b){
+    return operator === "a" ? add (a, b)
+        : operator === "s" ? subtract (a, b)
+        : operator === "m" ? multiply (a, b)
+        : operator === "d" ? divide (a, b)
+        : "Error"
+}
+
+//create function to populate display when number buttons are pressed
+//Concats button presses. Still need to wrtie to display div
+const write = (input) =>{
+    display === 0 ? display = input
+    :display = display.toString() + input
+}
+
+//create a variable for whats entered on the display
+
+//store display variable in num1 or 2 when operator is pushed
+
+//store the operator in variable when it is pushed
+
+//call the operate function when = is pressed, update the display with the answer
+
+//After this is working do more:
+//Add if/else arguments to make the operate function call if operator is pushed after num2
+//Add if/else argument to allow for changing operater if num2 has not been entered
+//Store result of operator in Num1
+//Round result decimals to 10 places
+//What to do with early = press?
+//Clear button - sets num1, num2, display, result and maybe others to 0. unapply operator. More?
+//Add snark for divide by 0
+//After calculation, new number press clears calculator
+
+
+//xtra - add decimal ability. Don't let them add more than 1
+//add backspace button
+//add keyboard support
